9557f010db9bcfc3ed57324b02f4ac58e851c6f1
Tiffany Liu <tiffany_l_liu@trendmicro.com>
2021-08-22 16:01:24 +0800

    Catch-all routes

--- pages/notes/[...params].jsx
+++ pages/notes/[...params].jsx
@@ -4,11 +4,12 @@ import { useRouter } from 'next/router';
 const Page = () => {
     const router = useRouter();
 
-    const { id } = router.query;
+    const { params } = router.query;
+    console.log(params);
 
     return (
         <h1>
-            Note {id}
+            Note
         </h1>
     )
 }
